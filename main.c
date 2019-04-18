ec847709bfa380ee3afeaa57aa90dba30e8d34d5
Fabio Germano <fabinho.germano13@gmail.com>
2019-04-17 22:07:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,31 @@
-#include <stido.h>
+#include <stdio.h>
 
 int main(void)
 {
-  int a, b;
+  float a, b;
+  
   char c[2];
   
   scanf("%f", &a);
   scanf("%f", &b);
-  scanf("%s", &c).;
+  scanf("%s", &c);
+  
+  float r;
+  
+  if(c[0] == '+')
+  {
+  	r = a + b;
+	printf("%f", r);		
+  }
+  else if(c[0] == '-')
+  {
+  	r = a - b;
+  	printf("%f", r);
+  }
+  else
+  {
+  	printf("Operacao invalida");
+  }
   
   return 0;
 }
